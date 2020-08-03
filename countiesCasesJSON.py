725ef07ebf27d20e2e3ef1be49321975a6854adc
MartinMashalov <52296303+MartinMashalov@users.noreply.github.com>
2020-08-03 12:59:40 -0400

    JSON for the counties cases

---
+++ countiesCasesJSON.py
@@ -0,0 +1,162 @@
+countiesJSON = {
+    "New York City": [
+        "230,410",
+        "23,012"
+    ],
+    "Suffolk County": [
+        "43,345",
+        "2,044"
+    ],
+    "Nassau County": [
+        "43,322",
+        "2,706"
+    ],
+    "Westchester County": [
+        "35,997",
+        "1,579"
+    ],
+    "Rockland County": [
+        "13,882",
+        "470"
+    ],
+    "Orange County": [
+        "11,108",
+        "404"
+    ],
+    "Erie County": [
+        "8,572",
+        "621"
+    ],
+    "Monroe County": [
+        "4,769",
+        "268"
+    ],
+    "Dutchess County": [
+        "4,520",
+        "162"
+    ],
+    "Onondaga County": [
+        "3,485",
+        "189"
+    ],
+    "Albany County": [
+        "2,521",
+        "170"
+    ],
+    "Oneida County": [
+        "2,087",
+        "110"
+    ],
+    "Ulster County": [
+        "2,028",
+        "49"
+    ],
+    "Sullivan County": [
+        "1,483",
+        "32"
+    ],
+    "Niagara County": [
+        "1,446",
+        "68"
+    ],
+    "Putnam County": [
+        "1,428",
+        "61"
+    ],
+    "Broome County": [
+        "1,054",
+        "77"
+    ],
+    "Schenectady County": [
+        "1,035",
+        "37"
+    ],
+    "Rensselaer County": [
+        "736",
+        "39"
+    ],
+    "Saratoga County": [
+        "732",
+        "5"
+    ],
+    "Columbia County": [
+        "522",
+        "43"
+    ],
+    "Madison County": [
+        "400",
+        "6"
+    ],
+    "Ontario County": [
+        "349",
+        "26"
+    ],
+    "Warren County": [
+        "300",
+        "31"
+    ],
+    "Orleans County": [
+        "295",
+        "41"
+    ],
+    "Steuben County": [
+        "292",
+        "29"
+    ],
+    "Greene County": [
+        "289",
+        "5"
+    ],
+    "Fulton County": [
+        "283",
+        "20"
+    ],
+    "Genesee County": [
+        "272",
+        "6"
+    ],
+    "St. Lawrence County": [
+        "262",
+        "4"
+    ],
+    "Washington County": [
+        "255",
+        "11"
+    ],
+    "Oswego County": [
+        "246",
+        "0"
+    ],
+    "Wayne County": [
+        "246",
+        "1"
+    ],
+    "Herkimer County": [
+        "241",
+        "3"
+    ],
+    "Chautauqua County": [
+        "232",
+        "5"
+    ],
+    "Tompkins County": [
+        "230",
+        "2"
+    ],
+    "Chenango County": [
+        "211",
+        "3"
+    ],
+    "Tioga County": [
+        "189",
+        "19"
+    ],
+    "Livingston County": [
+        "167",
+        "6"
+    ],
+    "Chemung County": [
+        "163",
+        "5"
+    ]
+}
